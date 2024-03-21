a012245cb5e92ec431381bd38770e37cd780bcd7
Joszyboy <135155340+Joszyboy@users.noreply.github.com>
2024-03-21 09:00:26 +0100

    Add files via upload

---
+++ Dart Data Types.dart
@@ -0,0 +1,14 @@
+void main(){
+  // There different data types in Dart. Data types are use to tell or define how a particular data or variable name is to be used. They include:
+  // Strings
+  // Number (This includes int and double)
+  // List
+  // Maps
+  // Booleans, Runes and Null
+// Examples of each data type below:
+String name = "Joshua"; // The variable name stores Joshua in a String format;
+int num1 = 20;  // The variable name stores 20 in an integer format;
+double num2 = 9.8; // The variable name 9.8 in a decimal format;
+List ages = [20, 23, 25, 27, 30]; // The variable name stores a list of student agaes in int format;
+Map student_ages = {"Joshua" : 90, "Alex" : 82}; // The variable name stores the name and ages of two students in a Map format;
+}
