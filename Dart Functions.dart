dd0ea8d21e5c17884b2d042331c9699df7ed5320
Joszyboy <135155340+Joszyboy@users.noreply.github.com>
2024-03-21 09:17:29 +0100

    Add files via upload

---
+++ Dart Functions.dart
@@ -0,0 +1,23 @@
+void addtwo(int num1, int num2){
+  int sum = num1 + num2;
+   print("The sum is $sum");
+
+void main(){
+  add(10, 20);
+
+void subtractTwo(int num1, int num2){
+  int difference = num1 - num2;
+   print("The difference is $difference");
+}
+
+void main(){
+  add(1200, 20);
+
+void multiplytwo(int num1, int num2){
+  int multiply = num1 * num2;
+   print("The product is $sum");
+}
+
+void main(){
+  add(10, 20);
+}
